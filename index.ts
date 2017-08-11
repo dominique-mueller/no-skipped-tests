4666ddda79a7e3bbbda68dea9727d759ec6b0894
Dominique Müller <dominique.m.mueller@gmail.com>
2017-08-11 14:10:30 +0200

    feat(*): Add error handling (#2)
    - Handle file reading errors
    - Handle unexpected TypeScript compiler errors
    - Catch rejected promises (removing NodeJS warnings)
    - Add options w/ default options and merge strategy

--- index.ts
+++ index.ts
@@ -6,13 +6,18 @@ import { NoSkippedTestsAnalyzer } from './src/no-skipped-tests-analyzer';
 
 // TODO: Extract into files
 
+export interface Options {
+	pattern?: string;
+}
+
 export function getFilesToAnalyze( pattern: string ): Promise<Array<string>> {
-	return new Promise<Array<string>>( ( resolve: ( files: Array<string> ) => void, reject: () => void ) => {
+	return new Promise<Array<string>>( ( resolve: ( files: Array<string> ) => void, reject: ( error: Error ) => void ) => {
 
 		glob( pattern, ( error, files: Array<string> ) => {
 
+			// Handle errors
 			if ( error ) {
-				// TODO: Handle error
+				reject( error );
 				return;
 			}
 
@@ -23,80 +28,108 @@ export function getFilesToAnalyze( pattern: string ): Promise<Array<string>> {
 	} );
 }
 
-async function run() {
+export const defaultOptions: Options = {
+	pattern: '**/*.spec.ts' // TODO: Include js, jsx, tsx specs ...
+};
 
-	// TODO: try-catch errors / catch rejected promise
+function run( options: Options = {} ): Promise<void> {
 
-	console.log( '' );
-	const log = ora( 'Searching files' ).start();
+	return new Promise<void>( async( resolve: () => void, reject: ( error?: Error ) => void ) => {
 
-	// TODO: Read pattern from argument, assume spec files (ts + js) by default
-	const files: Array<string> = await getFilesToAnalyze( '**/*.spec.ts' );
+		// Get options
+		const mergedOptions: Options = Object.assign( defaultOptions, options );
 
-	// Exit early if no files are found
-	if ( files.length === 0 ) {
-		log.stop();
-		console.log( chalk.white.bgYellow( ' WARNING ' ), 'No files found with the given pattern.' );
-		console.log( '' );
-		process.exit( 0 );
-		return;
-	}
+		// Get list of files (relative file paths, to be specific) matching the given pattern
+		const log = ora( 'Searching files' ).start();
+		let files: Array<string>;
+		try {
+			files = await getFilesToAnalyze( '**/*.spec.ts' );
+		} catch ( error ) {
+			log.stop();
+			reject( error );
+			return;
+		}
 
-	// Analyze all files (asynchronously, #perfmatters)
-	log.text = 'Analyzing files for skipped tests [0%]';
-	const analysisPromises: Array<Promise<any>> = [];
-	const numberOfFiles: number = files.length;
-	let finished: number = 0;
-	files.forEach( ( filePath: string ) => {
-		const noSkippedTestsAnalyzer: NoSkippedTestsAnalyzer = new NoSkippedTestsAnalyzer( filePath );
-		const promise: Promise<any> = noSkippedTestsAnalyzer.analyze();
-		promise.then( () => {
-			finished++;
-			log.text = `Analyzing files for skipped tests [${ Math.round( ( finished / numberOfFiles ) * 100 ) }%]`;
-		} );
-		analysisPromises.push( promise );
-	} );
+		// Exit early if no files are found
+		if ( files.length === 0 ) {
+			log.stop();
+			console.warn( chalk.white.bgYellow( ' WARNING ' ), `No files found using the given pattern ("${ options.pattern }").` );
+			resolve();
+			return;
+		}
 
-	const results: any = await Promise.all( analysisPromises );
-	const numberOfErrors: number = results.reduce( ( accumulatedValue: number, currentValue: any ) => {
-		return accumulatedValue + ( <Array<any>> currentValue.errors ).length;
-	}, 0 );
-
-	log.stop();
-
-	if ( numberOfErrors === 0 ) {
-
-		console.log( chalk.white.bgGreen( ' OK ' ), 'Everything is fine, all tests are active.' );
-		console.log( '' );
-		process.exit( 0 );
-
-	} else {
-
-		console.log( chalk.white.bgRed( ' ERROR ' ), `Seems that not all tests are active (${ numberOfErrors } issues found).` );
-		console.log( '' );
-
-		// Show errors for the same file beneath each other
-		results.forEach( ( fileResult: any ) => {
-
-			const numberOfFileErrors: number = fileResult.errors.length;
-			if ( numberOfFileErrors !== 0 ) {
-
-				fileResult.errors.forEach( ( error: any ) => {
-					console.log(
-						chalk.red( `        ${ fileResult.filePath } (${ error.line }:${ error.char }):` ),
-						chalk.white( `Found "${ error.identifier }"` )
-					);
+		// Analyze all files (asynchronously, #perfmatters)
+		log.text = 'Analyzing files for skipped tests [0%]';
+		const analysisPromises: Array<Promise<any>> = [];
+		const numberOfFiles: number = files.length;
+		let finished: number = 0;
+		files.forEach( ( filePath: string ) => {
+			const noSkippedTestsAnalyzer: NoSkippedTestsAnalyzer = new NoSkippedTestsAnalyzer( filePath );
+			const promise: Promise<any> = noSkippedTestsAnalyzer.analyze();
+			promise
+				.then( () => {
+					finished++;
+					log.text = `Analyzing files for skipped tests [${ Math.round( ( finished / numberOfFiles ) * 100 ) }%]`;
+				} )
+				.catch( ( error: Error ) => {
+					log.stop();
+					reject( error );
+					return;
 				} );
-
-			}
-
+			analysisPromises.push( promise );
 		} );
 
-		console.log( '' );
-		process.exit( 1 );
+		// Collect the results
+		let results: any = await Promise.all( analysisPromises );
+		const numberOfErrors: number = results.reduce( ( accumulatedValue: number, currentValue: any ) => {
+			return accumulatedValue + ( <Array<any>> currentValue.errors ).length;
+		}, 0 );
 
-	}
+		// Log results (success or error)
+		log.stop();
+		if ( numberOfErrors === 0 ) {
+
+			console.log( chalk.white.bgGreen( ' OK ' ), 'Everything is fine, all tests are active.' );
+			resolve();
+
+		} else {
+
+			console.log( chalk.white.bgRed( ' ERROR ' ), `Seems that not all tests are active (${ numberOfErrors } issues found).` );
+			console.log( '' );
+
+			// Show errors for the same file beneath each other
+			results.forEach( ( fileResult: any ) => {
+				const numberOfFileErrors: number = fileResult.errors.length;
+				if ( numberOfFileErrors !== 0 ) {
+					fileResult.errors.forEach( ( error: any ) => {
+						console.log(
+							chalk.red( `        ${ fileResult.filePath } (${ error.line }:${ error.char }):` ),
+							chalk.white( `Found "${ error.identifier }"` )
+						);
+					} );
+				}
+			} );
+
+			reject();
+
+		}
+
+	} );
 
 }
 
-run();
+console.log( '' );
+run( {
+	pattern: 'example/**/*.spec.ts'
+} )
+	.then( () => {
+		console.log( '' );
+		process.exit( 0 );
+	} )
+	.catch( ( error?: Error ) => {
+		if ( error ) {
+			console.error( chalk.red( 'AN UNEXPECTED ERROR OCCURED:' ), chalk.white( error.toString() ) );
+		}
+		console.log( '' );
+		process.exit( 1 );
+	} );
