825d6873d4f13ec6aa284cb0cbc62f16bd1232cb
Dominique Müller <dominique.m.mueller@gmail.com>
2017-08-15 15:07:09 +0200

    Add unit tests

---
+++ test/examples/it-errors/two.spec.js
@@ -0,0 +1,27 @@
+describe( 'My Object', () => {
+
+	describe( 'My Function Block', () => {
+
+		it( 'should do something', () => {
+			// Implementation
+		} );
+
+		it( 'should do another thing', () => {
+			// Implementation
+		} );
+
+	} );
+
+	describe( 'My Edge Cases', () => {
+
+		xit( 'should handle this', () => {
+			// Implementation
+		} );
+
+		xit( 'should handle that', () => {
+			// Implementation
+		} );
+
+	} );
+
+} );
---
+++ test/examples/no-errors/one.spec.ts
@@ -0,0 +1,15 @@
+describe( 'My Fancy Thing', () => { // should ignore fdescribe or xdescribe
+
+	beforeEach( () => {
+		// Implementation
+	} );
+
+	it( 'should instantiate', () => {
+		// Implementation
+	} );
+
+	it( 'should compare true with true', () => { // should ignore fit or xit
+		expect( true ).toBe( true );
+	} );
+
+} );
